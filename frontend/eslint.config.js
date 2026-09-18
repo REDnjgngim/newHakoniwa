0b20cf1b097e0df136315711e692807c073d074f
REDnjgngim <fbi0d9s4@gmail.com>
2026-09-18 22:06:27 +0900

    フロントエンドの環境構築

---
+++ frontend/eslint.config.js
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import reactPlugin from '@eslint-react/eslint-plugin';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        ignores: ['dist', '.vscode', '.idea', '*.config.js'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        ...reactPlugin.configs.recommended,
+        plugins: {
+            ...reactPlugin.configs.recommended.plugins,
+            'react-refresh': pluginReactRefresh,
+        },
+        rules: {
+            ...reactPlugin.configs.recommended.rules,
+            'react-refresh/only-export-components': 'off',
+        },
+    },
+    prettierConfig,
+];
